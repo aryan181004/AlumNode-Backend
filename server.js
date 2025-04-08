1b8802da4a8f384ec45d6c7ec9351c26836b310c
aryan181004 <aryan2004.chavan@gmail.com>
2025-04-09 00:27:53 +0530

    cors for frontend url

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:5173",
-//     ],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://alum-node-frontend.vercel.app",
+      "https://alumnode-backend.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
