b04810d6414e808cbd6efafde6ba1f409f04b63d
aryan181004 <aryan2004.chavan@gmail.com>
2025-04-09 00:11:36 +0530

    removed cors

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://d6a9-103-97-166-170.ngrok-free.app",
-    ],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//     ],
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
